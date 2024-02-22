3fe9c8e6378fa83747f0a0c4afddbeecc929c320
Sydnee <svwoodhouse@gmail.com>
2024-02-21 22:57:24 -0500

    Added auth flow

--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+require('./models/User')
+require('./services/passport')
 
-const app = express()
-app.get('/', (req, res)=> {
-    res.send({ hi: "there" })
-})
+mongoose.connect(keys.mongoURI)
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT)
+const app = express();
+
+// Tells express to make use of cookies in our application
+app.use(
+    cookieSession({
+        // How long cookie can exist before expiring in miliseconds (30 days below)
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        // Encrypts the cookie. Its an array so you can provide multiple keys
+        // And a key is randomly chosen
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
---
+++ server/services/passport.js
@@ -0,0 +1,48 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys')
+const mongoose = require('mongoose')
+
+// Load something into the model
+const User = mongoose.model('users')
+
+// User is the Mongoose model
+passport.serializeUser((user, done) => {
+    // user.id is the ID that is assigned to the record by MongoDB, not the profile ID
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then( user => {
+        // User is the user model pulled from MongoDB
+        done(null, user)
+    })
+})
+
+passport.use(new GoogleStrategy(
+    {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, 
+    (accessToken, refreshToken, profile, done) => {
+        // Searches through the records for one that has that profile.id
+        // It is an async action so it returns a promise
+        User.findOne({ googleID: profile.id }).then(existingUser => {
+            if(existingUser) {
+                // Already have a record with the given profile.id
+                // Passport wants to use the done function to know that the final 
+                // steps are complete
+                // It takes an err message and the user data
+                done(null, existingUser);
+            } else {
+                // We don't have a user record with this ID, make a new one
+                // Creates a new instance of a record and save it to the database
+                // It is an async function so we need to waiti until it resolves before calling
+                // the done function
+                new User({ googleID: profile.id }).save().then(user => done(null, user))
+            }
+        })
+        }
+    )   
+);
